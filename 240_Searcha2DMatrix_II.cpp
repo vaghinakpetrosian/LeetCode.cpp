030d5834776589d6e51ab8bf921deb30b4275be5
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2025-07-17 21:06:54 +0400

    Create 240_Searcha2DMatrix_II.cpp

---
+++ 240_Searcha2DMatrix_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        int row = 0, col = cols-1;
+        while(row < rows && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
+        }
+
+        return false;
+    }
+};
