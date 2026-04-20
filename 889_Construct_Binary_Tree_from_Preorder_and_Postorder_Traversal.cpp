e36cdeb7ff9cc754c9b9a63d6047ad42acca7a78
vaghinakpetrosian <147193790+vaghinakpetrosian@users.noreply.github.com>
2026-04-20 13:44:50 +0400

    Implement binary tree construction from preorder and postorder

---
+++ 889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    short pre = 0, post = 0;
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) 
+    {
+        TreeNode *root = new TreeNode(preorder[pre]);
+        ++pre;
+        if(root->val != postorder[post])
+        {
+            root->left = constructFromPrePost(preorder, postorder);
+        }
+        if(root->val != postorder[post])
+        {
+            root->right = constructFromPrePost(preorder, postorder);
+        }
+        ++post;
+        return root;
+    }
+};
