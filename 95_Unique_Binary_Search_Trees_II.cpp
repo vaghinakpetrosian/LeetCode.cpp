b11b7b122beffa472c989ed2a6c0d2a9b3e12d91
vaghinakpetrosian <147193790+vaghinakpetrosian@users.noreply.github.com>
2026-04-28 02:04:38 +0400

    Update 95_Unique_Binary_Search_Trees_II.cpp

--- 95_Unique_Binary_Search_Trees_II.cpp
+++ 95_Unique_Binary_Search_Trees_II.cpp
@@ -1,15 +1,48 @@
-/*
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-*/
+// /*
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+// */
 
+// class Solution {
+// public:
+//     vector<TreeNode*> generateTrees(int n) {
+//         if (n == 0) return {};
+//         return buildTrees(1, n);
+//     }
+
+// private:
+//     vector<TreeNode*> buildTrees(int start, int end) {
+//         vector<TreeNode*> res;
+//         if (start > end) {
+//             res.push_back(nullptr);
+//             return res;
+//         }
+//         for (int i = start; i <= end; ++i) 
+//         {
+//             vector<TreeNode*> leftSubtrees = buildTrees(start, i - 1);
+//             vector<TreeNode*> rightSubtrees = buildTrees(i + 1, end);
+//             for (TreeNode* left : leftSubtrees) {
+//                 for (TreeNode* right : rightSubtrees) 
+//                 {
+//                     TreeNode* root = new TreeNode(i);
+//                     root->left = left;
+//                     root->right = right;
+//                     res.push_back(root);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
 class Solution {
+    map<pair<int, int>, vector<TreeNode*>> memo;
+
 public:
     vector<TreeNode*> generateTrees(int n) {
         if (n == 0) return {};
@@ -18,16 +51,20 @@ public:
 
 private:
     vector<TreeNode*> buildTrees(int start, int end) {
-        vector<TreeNode*> res;
-        if (start > end) {
-            res.push_back(nullptr);
-            return res;
+        if(start > end) 
+            return {nullptr};
+
+        if(memo.count({start, end}))
+        {
+            return memo[{start, end}];
         }
+        vector<TreeNode*> res;
         for (int i = start; i <= end; ++i) 
         {
             vector<TreeNode*> leftSubtrees = buildTrees(start, i - 1);
             vector<TreeNode*> rightSubtrees = buildTrees(i + 1, end);
-            for (TreeNode* left : leftSubtrees) {
+            for (TreeNode* left : leftSubtrees) 
+            {
                 for (TreeNode* right : rightSubtrees) 
                 {
                     TreeNode* root = new TreeNode(i);
@@ -37,6 +74,6 @@ private:
                 }
             }
         }
-        return res;
+        return memo[{start, end}] = res;
     }
 };
